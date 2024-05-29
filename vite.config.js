0a0323064d50b16651c8dfd181604a8bfee63e2e
Marcos Stefano Aguilar Saltos <99378605+StefanoIG@users.noreply.github.com>
2024-05-28 22:33:16 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 contacto: resolve(__dirname, 'contacto.html'),
                 about: resolve(__dirname, 'about.html'),
-                servicios: resolve(__dirname, 'servicios.html'),
-                menu: resolve(__dirname, 'menu.html'),
-                bebidas: resolve(__dirname, 'bebidas.html'),
-                cervezas: resolve(__dirname, 'cervezas.html'),
-                cocteles: resolve(__dirname, 'cocteles.html'),
-                piqueos: resolve(__dirname, 'piqueos.html'),
+                servicios: resolve(__dirname, 'Servicios.html'),
+                menu: resolve(__dirname, 'Menu.html'),
+                bebidas: resolve(__dirname, 'Bebidas.html'),
+                cervezas: resolve(__dirname, 'Cervezas.html'),
+                cocteles: resolve(__dirname, 'Cocteles.html'),
+                piqueos: resolve(__dirname, 'Piqueos.html'),
                 
             }
         }
